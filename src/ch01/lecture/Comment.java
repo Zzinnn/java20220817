84ffda6180eda06a287c4febdfef77a009f66398
Zzinnn <wjwlek@gmail.com>
2022-08-17 12:13:41 +0900

    FIRST COMMIT

---
+++ src/ch01/lecture/Comment.java
@@ -0,0 +1,19 @@
+package ch01.lecture;
+
+public class Comment {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("자바는 쉽지 않다");
+//		System.out.println(); sysout ctrl + space
+//		주석: 프로그램 실행에 영향을 미치지 않음
+//		한줄주석
+		/*
+		  여러 줄 주석
+		 */
+//		단축키는 ctrl + /
+//		힌트 얻기 ctrl + space
+//		한 줄 지우기 ctrl + D
+//		코드 위아래로 옮기기 alt + up down key
+//		콘솔 출력 코드 : sysout /sout
+	}
+}
